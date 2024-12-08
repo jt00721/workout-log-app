236ea79a9f1408d08bc1e136dc6358479aa7ecd3
jt00721 <titusgloverj@gmail.com>
2024-12-08 20:26:04 +0000

    Update main function to initialise and seed sqlite database

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"path/filepath"
+
+	"github.com/jt00721/workout-log-app/backend/database"
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("../frontend")))
+	db := database.ConnectDb()
+	defer db.Close()
+
+	database.InitializeSchema(db)
+	// database.SeedDatabase(db)
+
+	fs := http.FileServer(http.Dir(filepath.Join("..", "frontend")))
+	http.Handle("/", fs)
+
+	log.Println("Server is running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
